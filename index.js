401d1734483d5847f13b52cdc56d6edf78ad305c
Sreeram <sreeramvenkat@yahoo.com>
2015-02-03 15:43:29 +0000

    checking out some functions in node

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var app = express();
 var fs = require('fs')
+var os = require('os')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   filedata = fs.readFileSync("index.html").toString()
+  filedata += " " + os.type() + " " + os.arch() 
   response.send(filedata)
 })
 
