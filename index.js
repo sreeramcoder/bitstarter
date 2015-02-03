4e4b763cf71c4137a67bea043541b72bff1677d9
Sreeram <sreeramvenkat@yahoo.com>
2015-02-03 16:01:49 +0000

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var fs = require('fs')
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
