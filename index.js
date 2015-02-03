e446b83bbae36f71683850098afc479ed08b3e49
Sreeram <sreeramvenkat@yahoo.com>
2015-02-03 15:32:49 +0000

    reading data from html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express();
+var fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  filedata = fs.readFileSync("index.html").toString()
+  response.send(filedata)
 })
 
 app.listen(app.get('port'), function() {
