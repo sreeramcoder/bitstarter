5c7f72f993238d4dc41d5cae7afc9624cac1f56b
Sreeram <sreeramvenkat@yahoo.com>
2015-02-04 01:29:34 +0000

    fix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express()
+var fs = require('fs')
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send("Hello World!  how are youo ")
+  filedata = fs.readFileSync("index.html").toString();
+  response.send("Hello World!  here is the file data: "+filedata)
 })
 
 app.listen(app.get('port'), function() {
